bb70ac2ffb1acd91983d9561a788263c283ae7c9
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-09 00:38:06 +0300

    added

---
+++ read.js
@@ -0,0 +1,6 @@
+const movies = [
+    {id:1,mov:"The time", isComplete:false},
+    {id:2, mov: "The king", isComplete:true},
+    {id:3, mov: "movi", isComplete:false },
+    {id:3, mov: "ty", isComplete:false },
+]
